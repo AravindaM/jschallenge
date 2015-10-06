5e08cc30bdc4d395e4edb39e34b4e3970434f483
Aravinda Madusanka <araliyanage@gmail.com>
2015-10-07 01:51:22 +0800

    Added new views and handling controllers for Showing available cars list and search cars.
    Car search is to be implemented.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,17 +7,48 @@
  * # MainCtrl
  * Controller of the jschallengeApp
  */
-angular.module('jschallengeApp')
+var mainApp = angular.module('jschallengeApp',['ngRoute']);
 
-.controller('MainCtrl', function($scope, $http) {
+mainApp.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    }).
+
+    when('/searchCars', {
+      templateUrl: 'views/searchCar.html',
+      controller: 'searchCarsController'
+    }).
+
+    when('/showCarsList', {
+      templateUrl: 'views/availableCars.html',
+      controller: 'availableCarsController'
+    }).
+
+    otherwise({redirectTo: '/'});
+
+}]);
+
+mainApp.controller('MainCtrl', function($scope, $http) {
+  //Nothing to do
+});
+
+
+mainApp.controller('availableCarsController', function($scope, $http) {
 
   // Query for a booking in 1 day from now, for 2 hours.
   var start = Date.now() + 24 * 3600 * 1000;
   var end = start + 2 * 3600 * 1000;
+
+  //var start = Date.now();
+  //var end = start + .5 * 3600 * 1000;
   var url = 'http://jschallenge.smove.sg/provider/1/availability?book_start=' + start + '&book_end=' + end;
 
   $http.get(url).success(function(result) {
-    console.log('Result from the API call:', result);
+    //console.log('Result from the API call:', result);
+    $scope.carLocations = result;
   }).error(function(err) {
     // Hum, this is odd ... contact us...
     console.error(err);
@@ -25,3 +56,42 @@ angular.module('jschallengeApp')
 
 
 });
+
+mainApp.controller('searchCarsController', function($scope, $http) {
+  //Nothing to do
+});
+
+mainApp.directive('myDatepicker', function () {
+  return {
+    require : 'ngModel',
+    link : function (scope, element, attrs, ngModelCtrl) {
+      $(function(){
+        element.datepicker({
+          showOn:"both",
+          changeYear:true,
+          changeMonth:true,
+          dateFormat:'yy-mm-dd',
+          maxDate: new Date(),
+          yearRange: '1920:2012',
+          onSelect:function (dateText, inst) {
+            ngModelCtrl.$setViewValue(dateText);
+            scope.$apply();
+          }
+        });
+      });
+    }
+  }
+});
+
+
+function MyCtrl($scope) {
+
+  $scope.userInfo = {
+    person: {
+      mDate: '1967-10-07'
+    }
+  };
+
+}
+
+
